6dacc0c2210c6e8071fe788b348bbb482bb0c7bf
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2023-05-19 20:59:11 +0200

    Initial Commit

---
+++ AppShackCC/AppShackCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppShackCCApp.swift
+//  AppShackCC
+//
+//  Created by Parsa on 2023-05-19.
+//
+
+import SwiftUI
+
+@main
+struct AppShackCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
